fc7e322b16a7a7070f795edaea8d88b21d428bd7
Trace <tracedelange@me.com>
2021-06-14 12:14:17 -0500

    commit tree 8f1d07b25d61de2d05db4e6ebee238caa46150f0

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.getElementById('text').textContent = "This is really cool!"
+  }); 
